5a7be0cecaf8d1bc6f0ccac505b35927c8d84c9f
Dan Morgan <hello@danny.codes>
2020-01-13 20:20:44 -0500

    Finish the character inventory exercise

--- tests/test_inventory_manager.py
+++ tests/test_inventory_manager.py
@@ -1,5 +1,21 @@
 from .context import exercises
 
+INVENTORY = {'Sword': 420, 'Shield': 69, 'something': 123}
+PRINTED_INVENTORY = """Inventory:
 
-def test_greet():
-    assert exercises.inventory_manager.greet() == 'hello'
+Sword 420
+Shield 69
+something 123"""
+
+ADD_TO_INVENTORY = ['Sword', 'Shield', 'Shield', 'cat', 'Shield', 'cat']
+UPDATED_INVENTORY = {'Sword': 421, 'Shield': 72, 'something': 123, 'cat': 2}
+
+
+def test_print_inventory():
+    assert exercises.inventory_manager.print_inventory(
+        INVENTORY) == PRINTED_INVENTORY
+
+
+def test_add_to_inventory():
+    assert exercises.inventory_manager.add_to_inventory(
+        INVENTORY, ADD_TO_INVENTORY) == UPDATED_INVENTORY
